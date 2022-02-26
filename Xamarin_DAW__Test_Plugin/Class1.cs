4a0a80f39479c334eae85521c097d456269e4fe9
smallville7123 <smallville7123@gmail.com>
2022-02-27 01:33:46 +1000

    test plugin for Xamarin DAW

---
+++ Xamarin_DAW__Test_Plugin/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin_DAW__Test_Plugin
+{
+    public class Class1 : Xamarin_DAW.Plugin
+    {
+        public override View onCreateView()
+        {
+            return new Button
+            {
+                Text = "I AM A PLUGIN BUTTON FROM A SEPERATE PROJECT!!!"
+            };
+        }
+    }
+}
